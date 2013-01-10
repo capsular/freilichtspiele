e5982b4e798414f02525675b1261d7f62360db83
andre <andre@capsule.ch>
2013-01-10 10:12:34 +0100

    small changes on webserver

--- sites/all/themes/fszo/js/home.js
+++ sites/all/themes/fszo/js/home.js
@@ -6,24 +6,37 @@
 
             
             $.backstretch(settings.homePage['bgurl'],{'centeredY':false,fade:1000});
+            
+            
+            $imgholdr = $('<table id="imgholdr" />').append($('<tr><td>')).prependTo($('body'))
+            
+            var $img = $('<img/>').hide()
+            $img.load(function(){
+                
+                var $loaded = $(this)
+                $loaded.appendTo($('td')).css({'width':'100%', 'height':'auto'}).attr('alt','Freilichtspiele Zürcher Oberland').fadeIn('slow')
+                $('<img/>').attr('src', settings.homePage['animurl'])
+                            .appendTo($('body')).css({'position':'absolute','left':'-200px','bottom':'10%'})
+                            .animate({
+                                left: '+=2000',
+                              }, 30000, function() {
+                              })
+            })
+            $img.attr('src', settings.homePage['txturl']);
+            $img.on('click', function(){
+                window.location.href=settings.homePage['linkto']
+            })
+            
+            
+            
+            
+            
+            
+            
+            
+            
             $(document.body).on("backstretch.show", function () { 
-                $imgholdr = $('<table id="imgholdr" />').append($('<tr><td>')).insertAfter($('.backstretch'))
-                var $img = $('<img/>').hide()
-                $img.load(function(){
-                    var $loaded = $(this)
-                    $loaded.appendTo($('td')).css({'width':'100%', 'height':'auto'}).attr('alt','Freilichtspiele Zürcher Oberland').fadeIn('slow')
-                    $('<img/>').attr('src', settings.homePage['animurl'])
-                                .appendTo($('body')).css({'position':'absolute','left':'-200px','bottom':'10%'})
-                                .animate({
-                                    left: '+=2000',
-                                  }, 50000, function() {
-                                    // Animation complete.
-                                  })
-                })
-                $img.attr('src', settings.homePage['txturl']);
-                $img.on('click', function(){
-                    window.location.href=settings.homePage['linkto']
-                })
+                
             });
                             
         }
